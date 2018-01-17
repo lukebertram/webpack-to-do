69853e26ccbac1c436e84afeb6a2282c56736e63
Guest User <Guest@Epicodus-4B.local>
2018-01-16 16:49:10 -0800

    make a webpack/cli version of the to-do list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,48 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  currentFocus: string = 'Angular Homework';
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
+  tasks: Task[] = [
+    new Task('Finish weekend Angular homework for Epicodus course', 3),
+    new Task('Begin brainstorming possible JavaScript group projects', 2),
+    new Task('Add README file to last few Angular repos on GitHub', 1)
+  ];
+
+  priorityColor(currentTask){
+    switch(currentTask.priority) {
+      case 3: {
+        return "bg-danger";
+      }
+      case 2: {
+        return "bg-warning";
+      }
+      default: {
+        return "bg-info";
+      }
+    }
+    
+  }
+
+  editTask() {
+    alert("You just made this popup happen. It's your own fault.")
+  }
+
+  isDone(clickedTask: Task) {
+    if(clickedTask.done){
+      console.log("This task is complete");
+    } else {
+      console.log("This task is not yet complete. You can just do it tomorrow.");
+    }
+  }
+}
+
+export class Task{
+  public done: boolean = false;
+  constructor(public description: string, public priority: number) { }
 }
